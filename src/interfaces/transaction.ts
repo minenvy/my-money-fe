b3903aaa955eef326b4bddf79422b27276fbfe92
MinhNV <minhnv@bravebits.vn>
2023-08-04 09:33:59 +0700

    fix error add draft transaction

--- src/interfaces/transaction.ts
+++ src/interfaces/transaction.ts
@@ -1,15 +1,16 @@
 export interface Transaction {
-	id: string
-	money: number
-	walletName: string
-	type: string
-	createdAt: Date
-	note?: string
-	image?: string | File
-	accessPermission: 'public' | 'private'
+  id: string
+  money: number
+  walletName: string
+  walletId: string
+  type: string
+  createdAt: Date
+  note?: string
+  image?: string | File
+  accessPermission: 'public' | 'private'
 }
 
 export interface MoneyType {
-	name: string
-	type: 'in' | 'out'
+  name: string
+  type: 'in' | 'out'
 }
